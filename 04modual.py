7062082b32e349362673f1ca4bab3364587d1828
sun12wook11 <sun12wook11@gmail.com>
2024-07-12 10:38:52 +0900

    연습문제 풀이

--- 04modual.py
+++ 04modual.py
@@ -39,10 +39,74 @@ print(dt.date.today())
 print(dt.datetime.now().strftime('%Y년 %m월 %d일 %H시 %M분 %S초'))
 
 # 긴급재난 지원금 대상자 판별
+incom = int(input('월 소득을 입력하세요: '))
+diff = int(input('타 지원금 수급 여부 수급1, 미수급2 :  '))
+
+# print ('수급대상자') if (incom <= 4000000) & (diff > 1) else print ('수급대상자 아님')
+result = '수급대상자' if (incom <= 4000000) & (diff > 1) else '수급대상자 아님'
+
+print (result)
+
+# 긴급재난 지원금 대상자 판별 (operator, 성능이 더 좋음)
+
+import operator as op
+incom = int(input('월 소득을 입력하세요: '))
+diff = int(input('타 지원금 수급 여부 수급1, 미수급2 :  '))
+tgIncome = 4_000_000
+
+result = '수급대상자' if op.and_(
+            op.le(incom, tgIncome),
+            op.eq(diff, 2)) else '수급대상자 아님'
+
+print (result)
+
 
 # 수온 계산기
 
+deeps = float(input('수심(m)은? '))
+tem = 20 - deeps * 0.07
+print(f'수심이 {deeps}m 일때 수온의 온도는 {tem}℃ 입니다.')
+
+# 수온 계산기(op)
+import operator as op
+deeps = float(input('수심(m)은? '))
+tem = 20 - deeps * 0.07
+print(f'수심이 {deeps}m 일때 수온의 온도는 {tem}℃ 입니다.')
+
+
 # 자동차 주행거리 계산
+import operator as op
+speed = float(input('주행속도는? '))
+rtime = int(input('주행시간은? '))
+
+# dist = speed * rtime
+dist = op.mul(speed,rtime)
+
+print(f'{dist} km')
+
 
 # 컴퓨터 업무 수량 파악
+worktime = int(input('근무시간은? '))
+needpc = 0
+if (24 % worktime) == 0:
+   needpc = 24 // worktime
+elif (24 % worktime) >= 0:
+    needpc = 24 // worktime + 1
 
+print(f'필요한 컴퓨터: {needpc}')
+
+worktime = int(input('근무시간은? '))
+comp = 3 * 8 // worktime
+etccomp = 1 if (3 * 8 % worktime) > 0 else 0
+print(f'필요한 컴퓨터: {comp + etccomp}')
+
+
+# 컴퓨터 업무 수량 파악 (operator)
+import operator as op
+worktime = int(input('근무시간은? '))
+
+cal1 = op.mul(3,8)
+comp = op.floordiv(cal1, worktime)
+etccomp = 1 if (op.mod(cal1,worktime) > 0) else 0
+
+print(f'필요한 컴퓨터: {comp + etccomp}')
