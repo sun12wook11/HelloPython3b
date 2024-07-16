382a8685e33d301069c4e4a9b829a0005f0a48e8
sun12wook11 <sun12wook11@gmail.com>
2024-07-16 14:17:05 +0900

    반복문 문제

--- 12loop.py
+++ 12loop.py
@@ -89,3 +89,81 @@ while w * h < 300:
     print(w,h,'삼각형의 넓이는 ', 1/2*(w * h),'입니다\n')
     w *= 2
     h *= 3
+
+
+# 기차 시간
+tA = 10
+tB = 25
+tC = 30
+min = 1
+
+while min < 541:
+    if min % 5 ==0:
+        if min % tA == 0 and min % tB == 0: # 50분 간격
+            hour = 9+ min //60
+            min = min % 60
+            print(f'{hour}시{min}분 교차')
+        elif min % 10 == 0 and min % 30 == 0: # 30분 간격
+            hour = 9+ min //60
+            min = min % 60
+            print(f'{hour}시{min}분 교차')
+        elif (min % 25 == 0) and (min % 30 == 0): # 150분 간격
+            hour = 9+ min //60
+            min = min % 60
+            print(f'{hour}시{min}분 교차')
+    min += 1
+
+
+# 로그인 기능 만들기
+attempts = 0
+max_attempts = 5
+correct_password = 'hanbitac'
+
+while True:
+    password = input("관리자 암호를 입력하세요: ")
+    if password == correct_password:
+        print("로그인 됐습니다.")
+        break
+    else:
+        attempts += 1
+        if attempts < max_attempts:
+            print("암호를 다시 확인하세요!")
+        else:
+            print("로그인 실패!! 횟수 초과!!!")
+
+# 반복문 내에 건너뛰기: continue
+# for, while문 내에서 반복흐름을 일시적으로 넘기기 위해 사용
+sum = 0
+for i in range(1,10+1):
+    if i%2 == 0 : continue
+    sum += 1
+print(sum)
+
+# 1~100까지 정수 합
+# 단 3이나 7의 배수는 뺌
+sum = 0
+i = 0
+while i < 100:
+    i += 1
+    if i % 3 == 0 or i % 7 == 0:
+        continue
+    else:
+        sum += i
+print(sum)
+
+
+sum = 0
+for i in range(1 ,100+1):
+    if i % 3 == 0 or i % 7 == 0:
+        continue
+    else:
+        sum += i
+print(sum)
+
+
+total_sum = 0
+for i in range(1, 101):
+    if i % 3 != 0 and i % 7 != 0:
+        total_sum += i
+
+print(total_sum)
