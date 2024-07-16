4a8e4fafb29a8e2bad770f94a00483f35c8feefc
sun12wook11 <sun12wook11@gmail.com>
2024-07-16 17:14:36 +0900

    dict 파이썬 자료구조

---
+++ 16dict.py
@@ -0,0 +1,58 @@
+# 딕셔너리
+# 이름(key)과 값(value)으로 구성된 연관배열의 일종
+# 자료구조를 만들때는 {}를 사용하고
+# 이름과 값은 :으로 구분해서 정의함
+# 다른 언어인  JSON과 유사한구조
+# 이름을 통해 자료를 찾는 헤시 테이블을 이용하므로 검색속도가 빠름
+# 순서와 상관없기에 빠름
+
+# 중간고사 성적을 dict로 선언
+sj = {'C/C++':'A', 'Java':'B+', '네트워크':'C', '보안':'A+', '해킹':'F', '클라우드':'C+'}
+
+print(sj)
+
+# 회원정보를 dict로 선언
+# key : 이름, 아이디, 비번, 이메일, 주소, 취미
+member = {'name':'홍길동',
+          'uid':'abc',
+          'pw':'123',
+          'em':'a@1',
+          'addr':'here',
+          'hobby':['운동','게임','여행']}
+print(member)
+
+# 딕셔너리 다루기
+# 조회 : 변수명[키], 변수명.get(키)
+print(member['uid'], member['pw'])
+print(member.get('em'),member.get('addr'))
+print(member.get('zipcode'))
+
+# 새로운 항목 추가 : 변수명['새로운키'] = 값을 지정
+member['zipcode'] = '12345'
+
+# 기존 항목 수정 : 변수명['키'] = 수정할값
+member['zipcode'] = '98765'
+member['addr'] = 'there'
+
+# 기존항목 삭제 : 변수명.pop(키)
+member.pop('zipcode')
+member.pop('addr')
+
+# dict의 모든 키 조회 : 변수명.keys()
+# dict의 모든 값 조회 : 변수명.values()
+
+print(member.keys())
+print(member.values())
+
+
+# dict 전체항목 조회 출력
+for k in member.keys():
+    print(f'{member[k]}', end=' ')
+
+# 열거형 enumerrate으로 dict 조회
+for idx, k in enumerate(member):
+    print(idx, k)
+
+# key와 value를 한번에 출력 : 변수명.items
+for k, v in member.items():
+    print(k,v)
