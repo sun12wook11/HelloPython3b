2bc21ce65c2f3a7fe05ef7aad2b49104195848a6
sun12wook11 <sun12wook11@gmail.com>
2024-07-19 18:19:43 +0900

    성적처리V7

---
+++ sunSfeed/sungjukv7.py
@@ -0,0 +1,58 @@
+# V7 데이터베이스화(sql)
+
+# 메뉴입력
+def displayMenu():
+    # 프로그램 메뉴 정의
+    main_menu = '''
+    ================== 
+     성적 프로그램 v5
+    ==================
+     1. 추가
+     2. 조회
+     3. 상세조회
+     4. 수정
+     5. 삭제 
+     0. 종료
+    ==================
+    '''
+    print(main_menu, end='')
+    menu = input('=> 메뉴를 선택하세요: ')
+    return menu
+
+# 기본 데이터 및 변수 선언
+sjs = [['일지매', 99, 87, 65, 251, 83.7, '우']]
+
+# 성적 데이터 리스트 입력 받기
+def readSungJuk():
+    sj = []
+    cnt = len(sjs)
+    sj.append(input(f"{cnt}번 학생 이름을 입력하세요: "))
+    sj.append(int(input(f"{cnt}번 학생 국어 점수를 입력하세요: ")))
+    sj.append(int(input(f"{cnt}번 학생 영어 점수를 입력하세요: ")))
+    sj.append(int(input(f"{cnt}번 학생 수학 점수를 입력하세요: ")))
+    return sj
+
+# 입력받은 성적 리스트 입력 처리 -> 총점 평균 학점
+# V7 데이터베이스 처리하는 코드로 바꾸자
+def addSungJuk(sj):
+    sj.append(sj[1] + sj[2] + sj[3])
+    sj.append(sj[4] / 3)
+    grd =   '수' if sj[5] >= 90 else \
+            '우' if sj[5] >= 80 else \
+            '미' if sj[5] >= 70 else \
+            '양' if sj[5] >= 60 else '가'
+    sj.append(grd)
+    sjs.append(sj)
+
+# 리스트에 저장된 성적 데이터 조회
+def showSungJuk():
+    result =''
+    for sj in sjs:
+        result += f'이름: {sj[0]}, 국어: {sj[1]}, 영어: {sj[2]}, 수학: {sj[3]}, 총점: {sj[4]}, 평균: {sj[5]:.2f}, 등급: {sj[6]}\n'
+    print(result)
+
+
+
+# 입력 받은거 성적처리 계산
+def computeSungJuk(sj):
+    pass
