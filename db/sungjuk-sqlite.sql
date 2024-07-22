2c51143218d9f7c097c6597025775a545ce4cf76
sun12wook11 <sun12wook11@gmail.com>
2024-07-22 10:52:19 +0900

    성적처리 프로그램 V7 - 데이터베이스 - getTotalSungJUk

---
+++ db/sungjuk-sqlite.sql
@@ -0,0 +1,39 @@
+-- 성적 테이블 만들기
+create table sungjuk
+(
+    sjno    integer
+        primary key autoincrement,
+    name    varchar(10) not null,
+    kor     int         not null,
+    eng     int         not null,
+    mat     int         not null,
+    tot     int         not null,
+    avg     decimal(5, 1),
+    grd     varchar(2),
+    -- regdate datetime default (datetime('now')) -- UTC
+    regdate datetime default (datetime('now','localtime')) -- UTC + 9
+);
+
+-- 성적 테이블 삭제
+drop table sungjuk;
+
+-- 성적 데이터 추가
+insert into sungjuk (name, kor, eng, mat, tot, avg, grd)
+values ('abc123',99,99,99,297,99.0,'수');
+
+-- 성적 데이터 검색
+select * from sungjuk;
+
+-- 성적 데이터 중 이름,국어,영어,수학만 조회
+select name, kor, eng, mat from sungjuk;
+
+-- 성적 데이터 중 이름,국어,영어,수학, 등록일만 조회
+select name, kor, eng, mat, substr(regdate,0,10) regdate
+from sungjuk;
+
+-- 성적 데이터 중 이름으로 검색해서 모두 조회
+select * from sungjuk where name = 'abc123';
+
+-- 성적 데이터 중 학생번호로 검색해서 모두 조회
+select * from sungjuk where sjno = 1;
+
--- sunSfeed/sungjukv7.py
+++ sunSfeed/sungjukv7.py
@@ -1,4 +1,6 @@
 # V7 데이터베이스화(sql)
+import sqlite3
+
 
 # 메뉴입력
 def displayMenu():
@@ -25,7 +27,7 @@ sjs = [['일지매', 99, 87, 65, 251, 83.7, '우']]
 # 성적 데이터 리스트 입력 받기
 def readSungJuk():
     sj = []
-    cnt = len(sjs)
+    cnt = getTotalSungJUk()
     sj.append(input(f"{cnt}번 학생 이름을 입력하세요: "))
     sj.append(int(input(f"{cnt}번 학생 국어 점수를 입력하세요: ")))
     sj.append(int(input(f"{cnt}번 학생 영어 점수를 입력하세요: ")))
@@ -35,6 +37,33 @@ def readSungJuk():
 # 입력받은 성적 리스트 입력 처리 -> 총점 평균 학점
 # V7 데이터베이스 처리하는 코드로 바꾸자
 def addSungJuk(sj):
+    computeSungJuk(sj)
+    newSungJuk(sj)
+
+
+# 리스트에 저장된 성적 데이터 조회
+def showSungJuk():
+    result =''
+    sjs = readAllSungJuk()
+    for sj in sjs:
+        result += f'이름: {sj[0]}, 국어: {sj[1]}, 영어: {sj[2]}, 수학: {sj[3]}, 총점: {sj[4]}, 평균: {sj[5]:.2f}, 등급: {sj[6]}\n'
+    print(result)
+
+
+def getTotalSungJUk():
+    sql = 'select count(sjno) + 1  total from sungjuk'
+    cnt = 0
+    conn = sqlite3.connect('db/python.db')
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    rs = cursor.fetchall()
+    for r in rs:
+        cnt = r[0]
+    cursor.close()
+    conn.close()
+    return cnt
+# 입력 받은거 성적처리 계산
+def computeSungJuk(sj):
     sj.append(sj[1] + sj[2] + sj[3])
     sj.append(sj[4] / 3)
     grd =   '수' if sj[5] >= 90 else \
@@ -42,17 +71,11 @@ def addSungJuk(sj):
             '미' if sj[5] >= 70 else \
             '양' if sj[5] >= 60 else '가'
     sj.append(grd)
-    sjs.append(sj)
 
-# 리스트에 저장된 성적 데이터 조회
-def showSungJuk():
-    result =''
-    for sj in sjs:
-        result += f'이름: {sj[0]}, 국어: {sj[1]}, 영어: {sj[2]}, 수학: {sj[3]}, 총점: {sj[4]}, 평균: {sj[5]:.2f}, 등급: {sj[6]}\n'
-    print(result)
+# 처리된 성적데이터를 테이블에 저장
+def newSungJuk(sj):
+    pass
 
-
-
-# 입력 받은거 성적처리 계산
-def computeSungJuk(sj):
+#
+def readAllSungJuk():
     pass
